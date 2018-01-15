1775ae875dca825fb81c51e3d9e27b0dabccc6da
James Carmichael <james@siteclick.co.uk>
2018-01-15 13:08:45 +0000

    Bump package.json version number, increase test coverage to error classes

---
+++ test/error.js
@@ -0,0 +1,32 @@
+
+
+const Lab = require('lab');
+
+const lab = Lab.script();
+const Code = require('code');
+const { ConfigError, ValidationError, NotFoundError } = require('../src/errors.js');
+
+lab.experiment('Test error classes', () => {
+  lab.test('Config error', async () => {
+    const error = new ConfigError('Test message');
+
+    Code.expect(error.name).to.equal('ConfigError');
+    Code.expect(error.message).to.equal('Test message');
+  });
+
+  lab.test('Validation error', async () => {
+    const error = new ValidationError('Test message');
+
+    Code.expect(error.name).to.equal('ValidationError');
+    Code.expect(error.message).to.equal('Test message');
+  });
+
+  lab.test('Not found error', async () => {
+    const error = new NotFoundError('Test message');
+
+    Code.expect(error.name).to.equal('NotFoundError');
+    Code.expect(error.message).to.equal('Test message');
+  });
+});
+
+exports.lab = lab;
