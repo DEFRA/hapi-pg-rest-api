53831ad393ce50de099863347825de07e37f2d6b
James Carmichael <james@siteclick.co.uk>
2018-11-30 16:23:46 +0000

    Replace error classes with es6-error

--- src/errors.js
+++ src/errors.js
@@ -2,48 +2,28 @@
  * Error classes
  * @module errors
  */
+const ExtendableError = require('es6-error');
 
 /**
  * An error class for when API class has been instantiated with incorrect
  * config object
  * @class ConfigError
  */
-class ConfigError extends Error {
-  constructor (message) {
-    super(message);
-    this.name = 'ConfigError';
-  }
-}
+class ConfigError extends ExtendableError {}
 
 /**
  * An error class for when HTTP request params/payload fail validation
  * @class ValidationError
  */
-class ValidationError extends Error {
-  constructor (message) {
-    super(message);
-    this.message = message;
-    this.name = 'ValidationError';
-  }
-}
+class ValidationError extends ExtendableError {}
 
 /**
  * An error class for when the requested data was not found in the DB
  * @class NotFoundError
  */
-class NotFoundError extends Error {
-  constructor (message) {
-    super(message);
-    this.name = 'NotFoundError';
-  }
-}
+class NotFoundError extends ExtendableError {}
 
-class NotImplementedError extends Error {
-  constructor (message) {
-    super(message);
-    this.name = 'NotImplementedError';
-  }
-}
+class NotImplementedError extends ExtendableError {}
 
 module.exports = {
   ConfigError,
