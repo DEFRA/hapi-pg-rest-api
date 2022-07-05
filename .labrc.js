606a89496673b0de7737c40061043decc7ba4698
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-07-05 23:15:28 +0100

    Implement changes

--- .labrc.js
+++ .labrc.js
@@ -1,11 +1,15 @@
+'use strict'
 // default settings for lab test runs.
 //
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
-  globals: 'globalThis',
   verbose: true,
-  'coverage-exclude': [
-    'node_modules',
-    'test'
-  ]
+  coverage: true,
+  // Means when we use *.only() in our tests we just get the output for what we've flagged rather than all output but
+  // greyed out to show it was skipped
+  'silent-skips': true,
+  // lcov reporter required for SonarCloud
+  reporter: ['console', 'html', 'lcov'],
+  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
+  globals: ['globalThis'].join(',')
 };
